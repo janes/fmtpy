0af24343505b3508ccfad4b29ff51f06f3a01cf0
Fábio <fabiomt92@hotmail.com>
2020-04-05 22:23:33 -0300

    pacote v3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='Realiza análises financeiras',
    author='Fábio Teixeira',
    author_email='fabiomt92@hotmail.com',
-   packages=['fmtpy', 'cvmpy'],  #same as name
+   packages=['fmtpy'],  #same as name
    install_requires=['selenium', 'bs4'], #external packages as dependencies
 )
 
