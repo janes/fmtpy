81f410bdebb6287aebd6569d1a008cb4a8ac6e6c
Fábio <fabiomt92@hotmail.com>
2020-05-10 19:08:14 -0300

    Beta

--- fmtpy/__init__.py
+++ fmtpy/__init__.py
@@ -9,6 +9,135 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+def help():
+    """
+    # Gerar balanços financeiros:
+    Método: balancos
+        - Paramentros:
+            - papel: Indicar o código da ação da empresa a qual deseja obter os balanços
+            - wdriver (não obrigatório): Indicar o caminho de um webdriver caso deseja obter os balanços direto do site da cvm
+            - cnn (não obrigatório): Indicar a conexão com um banco de dados caso já estiver carregado informações em um banco de dados
+
+            Se nem o wdriver nem o cnn forem indicados, os dados serão buscados no site investsite.com.br, no mesmo padrão da cvm
+
+        O método balancos retorna um objeto da classe Balancos:
+            Método: 
+                - get:
+                    - Parametros:
+                        ind: Indica qual o relatório desejado, conforme seguinte índice:
+                            1:'Resultado', 
+                            2:'Balanço Ativo', 
+                            3:'Balanço Passivo', 
+                            4:'Valor Adicionado', 
+                            5:'Fluxo de Caixa', 
+                            6:'Resultado Abrangente' (Este vale apenas para o site da cvm)
+                        ano: Ano do relatório desejado
+                        tri: Trimestre desejado
+                        ajustado (Boolean): Alguns relatórios vem com o valor agregado do ano inteiro (somando os trimestres anteriores) 
+                                            Indique True caso queira desagregar o valor. (o padrão é True).
+
+                    retorna os resultados em numpy Array.
+
+                - df:
+                    - Parametros:
+                        mesmos parametros do get, retorna os resultados em um pandas DataFrame.
+
+            Exemplo:
+                empresa = fmtpy.balanco('PETR4')
+                resultado = empresa.df(1,2019,4,True)
+                
+                retorna o relatório de resultados da Petrobras para o 4° trimestre de 2019 desagregado.
+
+
+    # Séries de ações:
+    Classe Series:
+        - Paramentros:
+            - papel: Indicar um ou mais* códigos da ação da empresa a qual deseja obter os balanços
+
+        Método:
+            - histórico:
+                - Parâmetros:
+                    data1: lista com o período inicial e final no formado AAAAMMDD (pode ser só o ano ou só o mês) 
+                        exemplo: se desejar o ano de 2019 inteiro, inserir [201901, 201912] ou apenas [2019]
+                    dataini: valor da data no mesmo formato acima indicando a data inicial
+
+                    obs: Se nenhum parametro for inserido, retornará todo o período disponível
+                
+        
+            - intraday:
+                retorna a série do dia atual por minuto (ou do último dia)
+
+
+            * Se apenas um papel for selecionado, retornará a classe Features. Os métodos estão descritos abaixo:
+                - gera_retornos:
+                    Adiciona a coluna coma série de retornos em percentual:
+                    - Parâmetros:
+                        tipo (padrão = 0): 0 gera o log retorno, 1 gera o retorno comum.
+                    
+                - medias_moveis:
+                    Adiciona a coluna com o calculo da média móvel
+                    Parâmetros:
+                        n: Indica o número de períodos
+
+                - simulacao:
+                    Gera o resultado financeiro que seria obtido seguindo uma média móvel.
+                    Parâmetros:
+                        n: Indica o número de períodos
+                        valor: Valor financeiro aplicado
+
+                - coefbeta:
+                    Gera o coeficiente beta do ativo no período.
+                    Parâmetros:
+                        tipo (padrão = 0): 0 gera o log retorno, 1 gera o retorno comum.
+
+                selecionando apenas algumas features:
+                    Exemplo:
+                        serie = fmtpy.Series(['PETR4']).historico()
+                        serie['date', 'price']
+                        # isso retorna apenas as featrues date e price 
+
+            * Se mais de um for selecionado, retornará a classe Serie. Essa série contem um conjunto de objetos Features.
+                                        Os métodos estão descritos abaixo:
+                - gera_retornos:
+                    Adiciona a coluna de retornos para todos os ativos
+                    - Parâmetros:
+                        tipo (padrão = 0): 0 gera o log retorno, 1 gera o retorno comum.
+
+                - medias_moveis:
+                    Adiciona a coluna com o calculo da média móvel para todos os ativos
+                    Parâmetros:
+                        n: Indica o número de períodos
+
+                - matriz_correl:
+                    Gera a matriz de correlação dos ativos.
+                    - Parâmetros:
+                        tipo (padrão = 0): 0 gera o log retorno, 1 gera o retorno comum.     
+
+                Selecionando apenas um ativo:
+                    Exemplo:
+                        ativos = fmtpy.Series(['PETR4', 'ABEV3']).historico()
+                        # ativos['PETR4'] retorna o equivalente a fmtpy.Series('PETR4').historico()
+
+
+    # Opções
+    Classe Opcoes:
+        Utiliza as mesmas classes das ações, mas contém apenas um método:
+            Método listar:
+                retorna a lista de opões do ativo
+
+            exemplo:
+                opcoes = fmtpy.Opcoes(['VALE3','PETR4']).listar()
+                opcoes['VALE3']
+                # retorna as opões de VALE3
+
+
+    # Sobre:
+    Em desenvolvimento por: Fábio Minutti Teixeira.
+    https://www.linkedin.com/in/fabiomt/
+    https://github.com/Fabiocke
+        
+    """
+
 def balancos(papel, wdriver=False, cnn=False):
     if cnn:
         return bi.Balanco(papel, cnn)
--- fmtpy/main.py
+++ fmtpy/main.py
@@ -9,6 +9,8 @@ from tabulate import tabulate
 from scipy.stats import norm
 from scipy.stats import pearsonr
 
+import pandas as pd
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 #  Encontra os dias úteis a partir da petrobras
@@ -44,6 +46,11 @@ class Features:
         val = [np.column_stack([self.__dict__[i] for i in features])][0] if len(features)>1 else self.__dict__[features[0]]
         features1 = [[i for i in features], val, self._ativo]
         return features1
+
+    def df(self):
+        matriz = self.df
+        return pd.DataFrame(matriz[1], columns=matriz[0])
+
 
 # Gera um objeto contendo um conjunto de objeto Features
 class Serie:
@@ -157,18 +164,18 @@ class Features_series(Features):
         dados, retornos = self._dados, np.array([self.retorno(precos[i],precos[i+1], tipo) for i in range(len(precos[:-1]))])
         return self.__class__(list(self.__dict__)[3:]+['retornos'], np.column_stack([dados, np.append(retornos,0)]), self._ativo) 
       
-    # Gera coluna com as curvas moveis
-    def curvas_moveis(self, n):
+    # Gera coluna com as médias moveis
+    def medias_moveis(self, n):
         precos = self.__dict__['price']
-        curvas = [sum(precos[i:(i+n)])/len(precos[i:(i+n)]) for i in range(len(precos)-(n-1))]
-        curvas = np.append(curvas, [0 for i in range(n-1)])
-        return self.__class__(list(self.__dict__)[3:]+['curva_movel'], np.column_stack([self._dados, curvas]), self._ativo)      
+        medias = [sum(precos[i:(i+n)])/len(precos[i:(i+n)]) for i in range(len(precos)-(n-1))]
+        medias = np.append(medias, [0 for i in range(n-1)])
+        return self.__class__(list(self.__dict__)[3:]+['media_movel'], np.column_stack([self._dados, medias]), self._ativo)      
         
-    # Simula o resultado de operações seguindo uma curva móvel
+    # Simula o resultado de operações seguindo uma média móvel
     def simulacao(self, n, valor):
-        curvas = self.curvas_moveis(n)['date','price','curva_movel'].np()
-        curvas[1] = np.column_stack([curvas[1], [i[1]>i[2] for i in curvas[1]]])
-        cv = curvas[1]
+        medias = self.medias_moveis(n)['date','price','media_movel'].np()
+        medias[1] = np.column_stack([medias[1], [i[1]>i[2] for i in medias[1]]])
+        cv = medias[1]
         cv[0][3]=0
         cv = np.array([cv[i] for i in range(len(cv)) if cv[i][3] not in [np.concatenate([cv,cv[-1:]])[i+1][3]]])
         if cv[-1][3] == 1:
@@ -191,30 +198,32 @@ class Features_series(Features):
         cov = np.cov(r, ribov)[0][1]
         beta = cov/np.var(ribov, ddof=1)
         return beta
-        
+    
+
 
 class SerieCotacoes(Serie):
     def __init__(self, dados):
         super().__init__(dados, Features_series)
         
     # Gera a matriz de correlação
-    def matriz_correl(self):
+    def matriz_correl(self, tipo=0):
         ativos = list(self.__dict__)[3:]
-        self.gera_retornos()
+        self.gera_retornos(tipo)
         retornos = [self.__dict__[i].retornos for i in ativos]
-        return [[[pearsonr(i[:min(len(i), len(j))-1], j[:min(len(i), len(j))-1])[0] for j in retornos] for i in retornos], self._ativos]
+        matriz = [[[pearsonr(i[:min(len(i), len(j))-1], j[:min(len(i), len(j))-1])[0] for j in retornos] for i in retornos], self._ativos]
+        return pd.DataFrame(matriz[0], columns = matriz[1], index=matriz[1])
 
     # Gera a coluna de retornos dia a dia
-    def gera_retornos(self):
+    def gera_retornos(self, tipo=0):
         for i in self._ativos:
             if 'retornos' not in self.__dict__[i].__dict__:
-                self.__dict__[i] = self.__dict__[i].gera_retornos()
+                self.__dict__[i] = self.__dict__[i].gera_retornos(tipo)
 
-    # Gera coluna de curvas moveis
-    def curvas_moveis(self, n):
+    # Gera coluna de médias moveis
+    def medias_moveis(self, n):
         for i in self._ativos:
-            if 'curva_movel' not in self.__dict__[i].__dict__:
-                self.__dict__[i] = self.__dict__[i].curvas_moveis(n)
+            if 'media_movel' not in self.__dict__[i].__dict__:
+                self.__dict__[i] = self.__dict__[i].medias_moveis(n)
 
 
 
